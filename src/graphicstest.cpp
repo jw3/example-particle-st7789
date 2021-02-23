ef74909ab275b3bb10df7cebcdd7397717c39aff
John Wass <wassj@ctc.com>
2021-02-23 06:08:41 -0500

    switch to the ST7789, MODE3

--- src/graphicstest.cpp
+++ src/graphicstest.cpp
@@ -40,16 +40,18 @@
 #define TFT_RST        6 // Or set to -1 and connect to Arduino RESET pin
 #define TFT_DC         5
 
+SYSTEM_MODE(MANUAL)
+
 // OPTION 1 (recommended) is to use the HARDWARE SPI pins, which are unique
 // to each board and not reassignable. For Arduino Uno: MOSI = pin 11 and
 // SCLK = pin 13. This is the fastest mode of operation and is required if
 // using the breakout board's microSD card.
 
 // For 1.44" and 1.8" TFT with ST7735 use:
-Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
+//Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 
 // For 1.14", 1.3", 1.54", and 2.0" TFT with ST7789:
-//Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
+Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
 
 
 // OPTION 2 lets you interface the display using ANY TWO or THREE PINS,
@@ -71,7 +73,7 @@ void setup(void) {
   Serial.print(F("Hello! ST77xx TFT Test"));
 
   // Use this initializer if using a 1.8" TFT screen:
-  tft.initR(INITR_BLACKTAB);      // Init ST7735S chip, black tab
+  // tft.initR(INITR_BLACKTAB);      // Init ST7735S chip, black tab
 
   // OR use this initializer if using a 1.8" TFT screen with offset such as WaveShare:
   // tft.initR(INITR_GREENTAB);      // Init ST7735S chip, green tab
@@ -83,7 +85,7 @@ void setup(void) {
   //tft.initR(INITR_MINI160x80);  // Init ST7735S mini display
 
   // OR use this initializer (uncomment) if using a 1.3" or 1.54" 240x240 TFT:
-  //tft.init(240, 240);           // Init ST7789 240x240
+  tft.init(240, 240, SPI_MODE3);
 
   // OR use this initializer (uncomment) if using a 2.0" 320x240 TFT:
   //tft.init(240, 320);           // Init ST7789 320x240
